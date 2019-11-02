fa899f1b5db306c854cbe81a53b39d821bff8116
pantelic-dusan <pantelic.dusan@protonmail.com>
2019-11-02 20:58:37 +0100

    Creating game board and block modules

--- script.js
+++ script.js
@@ -1,64 +1,113 @@
 'use strict'
 
-var gameBoard = document.getElementById('game-board');
-var gameContainer = document.getElementById('game-container');
-const gameBoardSize = {
-    cols: 10,
-    rows: 16 
-}
-var blockSize = 0;
-
 function sleep(miliseconds) {
     return new Promise(resolve => setTimeout(resolve, miliseconds));
 }
 
-window.onresize = resizeGameBoard;
+function blockModule() {
+    var row = 0;
+    var column = 0;
+    var color = 'white';
 
-function calculateBlockSize() {
-    var maxWidth = gameContainer.offsetWidth;
-    var maxHeight = gameContainer.offsetHeight;
-    blockSize = Math.floor(Math.min(maxHeight/(gameBoardSize.rows+1), maxWidth/(gameBoardSize.cols+1)));
-    if(blockSize < 1) {
-        blockSize = 1;
+    function setPosition(position) {
+        row = position[0];
+        column = position[1];
     }
-    console.log(maxHeight);
-}
+    
+    function getPosition() {
+        return [row, column];
+    }
 
-function initGameBoard() {
-    gameBoard.style.gridTemplateColumns = 'repeat(' + gameBoardSize.cols + ', 1fr)';
-    gameBoard.style.gridTemplateRows = 'repeat(' + gameBoardSize.rows + ', 1fr)';
-    gameBoard.style.width = blockSize*gameBoardSize.cols + 'px';
-}
+    function setColor(color) {
+        this.color = color;
+    }
 
-function drawGameBoard() {
-    for(let i=0; i<gameBoardSize.cols; i++) {
-        for(let j=0; j<gameBoardSize.rows; j++) {
-            var node = document.createElement('i');
-            node.setAttribute('id', 'block-' + i + '-' + j);
-            node.setAttribute('class', 'fas fa-square');
-            node.style.fontSize = blockSize.toString() + 'px';
-            node.style.opacity = '0.5';
-            gameBoard.appendChild(node);
+    function getColor() {
+        return this.color;
+    }
+
+    var publicAPI = {
+        setPosition: setPosition,
+        getPosition: getPosition,
+        setColor: setColor,
+        getColor: getColor
+    }
+
+    return publicAPI;
+};
+
+var gameBoard = (function gameBoardModule() {
+    var gameContainerElement;
+    var gameBoardElement;
+    var blockSize;
+    var numOfColumns;
+    var numOfRows;
+    var activeBlocks;
+    
+    function initGameBoard() {
+        gameContainerElement = document.getElementById('game-container');
+        gameBoardElement = document.getElementById('game-board');
+        numOfColumns = 10;
+        numOfRows = 16;
+        calculateBlockSize();
+        gameBoardElement.style.gridTemplateColumns = 'repeat(' + numOfColumns + ', 1fr)';
+        gameBoardElement.style.gridTemplateRows = 'repeat(' + numOfRows + ', 1fr)';
+        gameBoardElement.style.width = blockSize*numOfColumns + 'px';
+        drawEmptyBoard();
+        activeBlocks = [];
+        window.onresize = resizeGameBoard();
+    }
+
+    function calculateBlockSize() {
+        var maxWidth = gameContainerElement.offsetWidth;
+        var maxHeight = gameContainerElement.offsetHeight;
+        blockSize = Math.floor(Math.min(maxHeight/(numOfRows+1), maxWidth/(numOfColumns+1)));
+        if(blockSize < 1) {
+            blockSize = 1;
         }
     }
-}
 
-function resizeGameBoard() {
-    calculateBlockSize();
-    initGameBoard();
-    for(let i=0; i<gameBoardSize.cols; i++) {
-        for(let j=0; j<gameBoardSize.rows; j++) {
-            var block = document.getElementById('block-' + i + '-' + j);
-            if(block != null) {
-                block.style.fontSize = blockSize.toString() + 'px';
+    function drawEmptyBoard() {
+        for(let i=0; i<numOfColumns; i++) {
+            for(let j=0; j<numOfRows; j++) {
+                let node = document.createElement('i');
+                node.setAttribute('id', 'block-' + i + '-' + j);
+                node.setAttribute('class', 'fas fa-square');
+                node.style.fontSize = blockSize.toString() + 'px';
+                node.style.color = 'white';
+                node.style.opacity = '0.5';
+                gameBoardElement.appendChild(node);
             }
         }
     }
-}
 
-function updateGameBoard() {
+    function resizeGameBoard() {
+        calculateBlockSize();
+        gameBoardElement.style.gridTemplateColumns = 'repeat(' + numOfColumns + ', 1fr)';
+        gameBoardElement.style.gridTemplateRows = 'repeat(' + numOfRows + ', 1fr)';
+        gameBoardElement.style.width = blockSize*numOfColumns + 'px';
+        for(let i=0; i<numOfColumns; i++) {
+            for(let j=0; j<numOfRows; j++) {
+                let block = document.getElementById('block-' + i + '-' + j);
+                if(block != null) {
+                    block.style.fontSize = blockSize.toString() + 'px';
+                }
+            }
+        }
+    }
 
-}
+    function updateGameBoard() {
+
+    }
+
+    var publicAPI = {
+        init: initGameBoard,
+        update: updateGameBoard
+    }
+
+    return publicAPI;
+})();
+
 
 function gameEnd() {
     return false;
@@ -66,22 +115,17 @@ function gameEnd() {
 
 function gameLoop() {
     if(!gameEnd()){
-        updateGameBoard();
+        gameBoard.update();
         sleep(100).then(() => {
             gameLoop();
         });
     }
 }
 
-function startGame() {
-    gameLoop();
-}
 
 function main() {
-    calculateBlockSize();
-    initGameBoard();
-    drawGameBoard();
-    startGame();
+    gameBoard.init();
+    gameLoop();
 }
 
 main();
